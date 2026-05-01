2bc09fa348d1f609f19b84cdaaaf9bf785f43b55
Collei <collei@collei.com.br>
2026-05-01 16:59:12 -0300

    changed class to more utility

--- src/Collei/Collections/Exceptions/CollectionException.php
+++ src/Collei/Collections/Exceptions/CollectionException.php
@@ -3,11 +3,21 @@ namespace Collei\Collections\Exceptions;
 
 use Throwable;
 use RuntimeException;
+use Collei\Collections\Collection;
 
 class CollectionException extends RuntimeException
 {
-    public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
+    protected $source;
+
+    public function __construct(Collection $source, string $message = '', int $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
+
+        $this->source = $source;
+    }
+
+    public function source()
+    {
+        return $this->source;
     }
 }
