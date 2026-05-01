f398b596e6bf2b2d3bfe8e15557c61ab0197d940
Collei <collei@collei.com.br>
2026-05-01 18:47:38 -0300

    refactor in methods

--- src/Collei/Collections/Traits/EnumeratesValues.php
+++ src/Collei/Collections/Traits/EnumeratesValues.php
@@ -3,19 +3,23 @@ namespace Collei\Collections\Traits;
 
 trait EnumeratesValues
 {
-    protected function useAsCallable($callabck)
+    protected function useAsCallable($callback)
     {
         return ! is_string($callback) && is_callable($callback);
     }
 
-    protected function valueRetriever($value)
+    protected function valueRetriever($value = null)
     {
+        if (is_null($value)) {
+            return $this->identity();
+        }
+
         if ($this->useAsCallable($value)) {
             return $value;
         }
 
         return function ($item) use ($value) {
-            return $item[$value] ?? $item->$value ?? $value;
+            return is_object($item) ? ($item->$value ?? null) : ($item[$value] ?? null);
         };
     }
 
@@ -35,7 +39,7 @@ trait EnumeratesValues
 
     public function identity()
     {
-        return function($value) {
+        return function($value, $key = null) {
             return $value;
         };
     }
