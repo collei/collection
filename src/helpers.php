c533209d896a9809650de5470bee6ec138bd1041
Collei <collei@collei.com.br>
2026-05-04 21:47:17 -0300

    added value(), enum_value(), is_enum() and is_backed_enum()

--- src/helpers.php
+++ src/helpers.php
@@ -17,3 +17,40 @@ function has_closure_arg_count(Closure $callback, int $count, bool $requiredOnly
         ? $reflect->getNumberOfRequiredParameters()
         : $reflect->getNumberOfParameters());
 }
+
+function value($value, ...$args)
+{
+    return ($value instanceof Closure) ? $value(...$args) : $value;
+}
+
+function enum_value($value, $default = null)
+{
+    if (is_backed_enum($value)) {
+        return $value->value;
+    }
+
+    if (is_enum($value)) {
+        return $value->name;
+    }
+
+    return $value ?? value($default);
+}
+
+function is_enum($object)
+{
+    if (version_compare(PHP_VERSION, '8.1.0', '>=')) {
+        return $object instanceof UnitEnum;
+    }
+
+    return false;
+}
+
+function is_backed_enum($object)
+{
+    if (version_compare(PHP_VERSION, '8.1.0', '>=')) {
+        return $object instanceof BackedEnum;
+    }
+
+    return false;
+}
+
