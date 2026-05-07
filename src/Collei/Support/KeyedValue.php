445174ea0e91c1ae01d305e27533c6deffe41a9f
Collei <collei@collei.com.br>
2026-05-06 22:16:09 -0300

    implememnted Arrayable interface, and added methods

--- src/Collei/Support/KeyedValue.php
+++ src/Collei/Support/KeyedValue.php
@@ -6,7 +6,7 @@ use RuntimeException;
 /**
  * Embodies a key-value pair.
  */
-class KeyedValue
+class KeyedValue implements Arrayable
 {
     /**
      * @var mixed
@@ -27,6 +27,16 @@ class KeyedValue
     public function __construct($value, $key)
     {
         [$this->value, $this->key] = [$value, $key];
+    }
+
+    /**
+     * Returns the instance as array.
+     * 
+     * @return array
+     */
+    public function toArray()
+    {
+        return [$this->key, $this->value];
     }
 
     /**
@@ -73,10 +83,34 @@ class KeyedValue
         }
         
         foreach ($classes as $class) {
-            return $this->value instanceof $class;
+            if ($this->value instanceof $class) {
+                return true;
+            }
         }
 
         return false;
+    }
+
+    /**
+     * Returns if the underlying value is an object and has such property.
+     * 
+     * @param string $property
+     * @return bool
+     */
+    public function hasProperty(string $property)
+    {
+        return is_object($this->value) && property_exists($this->value, $property);
+    }
+
+    /**
+     * Returns if the underlying value is an object and has such method.
+     * 
+     * @param string $method
+     * @return bool
+     */
+    public function hasMethod(string $method)
+    {
+        return is_object($this->value) && method_exists($this->value, $method);
     }
 
     /**
@@ -87,7 +121,7 @@ class KeyedValue
      */
     public function __get(string $name)
     {
-        if (is_object($this->value) && property_exists($this->value, $name)) {
+        if ($this->hasProperty($name)) {
             return $this->value->$name;
         }
 
@@ -104,7 +138,7 @@ class KeyedValue
     public function __call(string $method, array $parameters)
     {
         if (is_object($this->value)) {
-            if (method_exists($object, $method) || method_exists($object, '__call')) {
+            if (method_exists($this->value, $method) || method_exists($this->value, '__call')) {
                 return $this->value->{$method}(...$arguments);
             }
 
