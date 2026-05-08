eadd290d2049dbd67d8947692b34cdb16ef7b4d9
Collei <collei@collei.com.br>
2026-05-07 21:54:02 -0300

    added docblocks

--- src/Collei/Collections/Exceptions/CollectionException.php
+++ src/Collei/Collections/Exceptions/CollectionException.php
@@ -5,10 +5,24 @@ use Throwable;
 use RuntimeException;
 use Collei\Collections\CollectionInterface;
 
+/**
+ * Exceptions related to the collection.
+ */
 class CollectionException extends RuntimeException
 {
+    /**
+     * @var CollectionInterface
+     */
     protected $source;
 
+    /**
+     * Initialization.
+     * 
+     * @param Collei\Collections\CollectionInterface $source
+     * @param string $message = ''
+     * @param int $code = 0
+     * @param \Throwable $previous = null
+     */
     public function __construct(CollectionInterface $source, string $message = '', int $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
@@ -16,6 +30,11 @@ class CollectionException extends RuntimeException
         $this->source = $source;
     }
 
+    /**
+     * Returns the related collection.
+     * 
+     * @return Collei\Collections\CollectionInterface
+     */
     public function source()
     {
         return $this->source;
