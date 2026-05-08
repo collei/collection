0658fb905ee45cec18df75d8facfe9d0e648a51a
Collei <collei@collei.com.br>
2026-05-08 20:42:17 -0300

    added constructor on exception class

--- src/Collei/Collections/Exceptions/ItemNotFoundException.php
+++ src/Collei/Collections/Exceptions/ItemNotFoundException.php
@@ -6,5 +6,20 @@ namespace Collei\Collections\Exceptions;
  */
 class ItemNotFoundException extends CollectionException
 {
-    //
+    /**
+     * Initialization.
+     * 
+     * @param Collei\Collections\CollectionInterface $source
+     * @param string $message = ''
+     * @param int $code = 0
+     * @param \Throwable $previous = null
+     */
+    public function __construct(CollectionInterface $source, string $message = '', int $code = 0, Throwable $previous = null)
+    {
+        if (empty($message)) {
+            $message = sprintf('No item found in this collection');
+        }
+
+        parent::__construct($source, $message, $code, $previous);
+    }
 }
