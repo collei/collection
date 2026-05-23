b5db354c720674411f3c240878a3f67e47fef0f9
Collei <collei@collei.com.br>
2026-05-23 17:32:23 -0300

    made helper function declarations conditional to avoid hard conflict with other libraries

--- src/helpers.php
+++ src/helpers.php
@@ -1,83 +1,110 @@
 <?php
 
-/**
- * Performs deep getting of value using dot notation.
- * 
- * @param mixed $target
- * @param int|string|array|null $key
- * @param mixed $default = null
- * @return mixed
- */
-function deep_get($target, $key, $default = null)
-{
-    if (is_null($key)) {
-        return $target;
-    }
-
-    $key = is_array($key) ? $key : explode('.', $key);
-
-    foreach ($key as $i => $segment) {
-        unset($key[$i]);
-
-        if (is_null($segment)) {
+if (! function_exists('deep_get')) {
+    /**
+     * Performs deep getting of value using dot notation.
+     * 
+     * @param mixed $target
+     * @param int|string|array|null $key
+     * @param mixed $default = null
+     * @return mixed
+     */
+    function deep_get($target, $key, $default = null)
+    {
+        if (is_null($key)) {
             return $target;
         }
 
-        if ($segment === '*') {
-            return ($target instanceof Closure) ? $target() : $target;
+        $key = is_array($key) ? $key : explode('.', $key);
+
+        foreach ($key as $i => $segment) {
+            unset($key[$i]);
+
+            if (is_null($segment)) {
+                return $target;
+            }
+
+            if ($segment === '*') {
+                return ($target instanceof Closure) ? $target() : $target;
+            }
+
+            if ((is_array($target) || $target instanceof ArrayAccess) && array_key_exists($segment, $target)) {
+                $target = $target[$segment];
+            } elseif (is_object($target) && isset($target->{$segment})) {
+                $target = $target->{$segment};
+            } else {
+                return ($default instanceof Closure) ? $default() : $default;
+            }
         }
 
-        if ((is_array($target) || $target instanceof ArrayAccess) && array_key_exists($segment, $target)) {
-            $target = $target[$segment];
-        } elseif (is_object($target) && isset($target->{$segment})) {
-            $target = $target->{$segment};
-        } else {
-            return ($default instanceof Closure) ? $default() : $default;
+        return $target;
+    }
+}
+
+if (! function_exists('closure_count_required_args')) {
+    /**
+     * Retrieve how many arguments the given closure requires.
+     * 
+     * @return int
+     */
+    function closure_count_required_args(Closure $callback)
+    {
+        $refl = new ReflectionFunction($callback);
+
+        return $refl->getNumberOfRequiredParameters();
+    }
+}
+
+if (! function_exists('closure_count_args')) {
+    /**
+     * Retrieve how many arguments the given closure accepts.
+     * 
+     * @return int
+     */
+    function closure_count_args(Closure $callback)
+    {
+        $refl = new ReflectionFunction($callback);
+
+        return $refl->getNumberOfParameters();
+    }
+}
+
+if (! function_exists('method_count_required_args')) {
+    /**
+     * Retrieve how many arguments the given class method requires.
+     * Returns null if the method does not exist.
+     * 
+     * @return int|null
+     */
+    function method_count_required_args(object $object, string $methodName)
+    {
+        $refl = new ReflectionClass($object);
+
+        if (! $refl->hasMethod($methodName)) {
+            return null;
         }
+
+        return $refl->getMethod($methodName)->getNumberOfRequiredParameters();
     }
-
-    return $target;
 }
 
-/**
- * Retrieve how many arguments the given closure requires.
- * 
- * @return int
- */
-function closure_count_required_args(Closure $callback)
-{
-    $refl = new ReflectionFunction($callback);
+if (! function_exists('method_count_args')) {
+    /**
+     * Retrieve how many arguments the given class method accepts.
+     * Returns null if the method does not exist.
+     * 
+     * @return int|null
+     */
+    function method_count_args(object $object, string $methodName)
+    {
+        $refl = new ReflectionClass($object);
 
-    return $refl->getNumberOfRequiredParameters();
-}
+        if (! $refl->hasMethod($methodName)) {
+            return null;
+        }
 
-/**
- * Retrieve how many arguments the given closure accepts.
- * 
- * @return int
- */
-function closure_count_args(Closure $callback)
-{
-    $refl = new ReflectionFunction($callback);
-
-    return $refl->getNumberOfParameters();
-}
-
-/**
- * Retrieve how many arguments the given class method requires.
- * Returns null if the method does not exist.
- * 
- * @return int|null
- */
-function method_count_required_args(object $object, string $methodName)
-{
-    $refl = new ReflectionClass($object);
-
-    if (! $refl->hasMethod($methodName)) {
-        return null;
+        return $refl->getMethod($methodName)->getNumberOfParameters();
     }
-
-    return $refl->getMethod($methodName)->getNumberOfRequiredParameters();
 }
 
 /**
